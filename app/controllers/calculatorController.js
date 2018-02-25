7656ae4cf6c70e087ed46365d02b24c1d91760d4
chetans9 <chetanshenai9@gmail.com>
2018-02-25 21:22:39 +0530

    Calculate floating point numbers

--- app/controllers/calculatorController.js
+++ app/controllers/calculatorController.js
@@ -9,38 +9,67 @@ app.controller('calculatorController', function($scope,myCalc) {
     $scope.pendingOperation = null;
 
     // Holds the running total as numbers are added/subtracted
-    $scope.currentTotal = null;
+    $scope.currentTotal = 0;
+
+    $scope.pointerExists = 0;
 
     //whenever user presses button.
     $scope.enterInput = function(value) {
-        //if initial and nextNumber
+
+    	
+    	
+    	if(value == "." && $scope.pointerExists){
+    		return false;
+    	}
+    	if(value == "."){
+    		$scope.pointerExists = 1;
+    	}
+    	
+        //if initial or want to enter nextNumber 
         if ($scope.ledDisplay == "0" || $scope.nextNumber) {
-        	//Update Led output
             $scope.ledDisplay = value;
             $scope.nextNumber = false;
+
         } else {
+        	//Build input Stream on LED.
             $scope.ledDisplay += String(value);
         }
-        //Whenver the number is entered without operation. It is a pending number.
+
+
+        
+        
+
+        //Set Pending value for Calculations
         $scope.pendingValue = parseFloat($scope.ledDisplay);
     };
 
 
+    /**
+    * Whenever User clicks on operations like "+","-" etc.
+    */
     $scope.operate = function(method) {
-
         if ($scope.pendingValue) {
+
         	$scope.currentTotal = myCalc.calculate($scope);
+        	//Once caculated. Do not calcualte again.
+    		$scope.pendingValue = null;
+    		//Set pending operation to passed method
+    		$scope.pendingOperation = method;
+    		$scope.pointerExists = 0;
+        }
+        else{
+
+        	//No previously entered Number
         }
 
         //Show current total to led display
         $scope.ledDisplay = $scope.currentTotal;
-        //Set pending operation to passed method
-        $scope.pendingOperation = method;
+        
+        
         //Let user enter next number/pending value.
         $scope.nextNumber = true;
-
-
     };
+
 
 
     $scope.getResult = function(){
@@ -50,19 +79,26 @@ app.controller('calculatorController', function($scope,myCalc) {
     	//set pending oerartion to null
     	$scope.pendingOperation = null;
     	$scope.pendingValue = $scope.currentTotal;
+    	$scope.pointerExists = 0;
+    	$scope.nextNumber = null;
     };
 
     $scope.allClear = function() {
     $scope.currentTotal = null;
     $scope.pendingValue = null;
     $scope.pendingOperation = null;
-    $scope.ledDisplay = '0';
+    $scope.ledDisplay = 0;
+    $scope.pointerExists = 0;
     
   };
 
   $scope.clearOne = function(){
 
   	$scope.ledDisplay = $scope.ledDisplay.slice(0, -1);
+  	if($scope.ledDisplay.length == 0){
+  		$scope.ledDisplay = '0';
+  		$scope.pointerExists = 0;
+  	}
   	$scope.pendingValue = parseInt($scope.ledDisplay);
 
 
--- app/services/calculate.js
+++ app/services/calculate.js
@@ -1,24 +1,26 @@
 app.service('myCalc', function() {
 
     this.calculate = function ($scope){
-        if($scope.currentTotal && $scope.pendingOperation == 'add'){
+        if($scope.pendingOperation == 'add'){
         	return $scope.currentTotal+$scope.pendingValue;
         }
-        if($scope.currentTotal && $scope.pendingOperation == 'subtract'){
+        if($scope.pendingOperation == 'subtract'){
         	return $scope.currentTotal - $scope.pendingValue;
         }
-        if($scope.currentTotal && $scope.pendingOperation == 'multiply'){
+        if($scope.pendingOperation == 'multiply'){
         	return $scope.currentTotal * $scope.pendingValue;
         }
-        if($scope.currentTotal && $scope.pendingOperation == 'divide'){
+        if($scope.pendingOperation == 'divide'){
         	return $scope.currentTotal / $scope.pendingValue;
         }
-        if($scope.currentTotal && $scope.pendingOperation == 'mod'){
+        if($scope.pendingOperation == 'mod'){
         	return $scope.currentTotal % $scope.pendingValue;
         }
         else{
-        	//If We are Still Here that means user has entered his pending value
+        	//If We are Still Here that means user has entered his pending value and pressed operation once
+            
         	return $scope.pendingValue;
+
         }
     }
 
