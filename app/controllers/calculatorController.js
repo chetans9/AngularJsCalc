e6ee6afb29d9f30cf6c9319e39aaad7cb9e1378c
chetans9 <chetanshenai9@gmail.com>
2018-02-26 12:38:42 +0530

    Added operation Icon next to led

--- app/controllers/calculatorController.js
+++ app/controllers/calculatorController.js
@@ -13,6 +13,8 @@ app.controller('calculatorController', function($scope,myCalc) {
 
     $scope.pointerExists = 0;
 
+    $scope.operationIcon = null;
+
     //whenever user presses button.
     $scope.enterInput = function(value) {
 
@@ -28,6 +30,7 @@ app.controller('calculatorController', function($scope,myCalc) {
         if ($scope.ledDisplay == 0 || $scope.nextNumber) {
             $scope.ledDisplay = value;
             $scope.nextNumber = false;
+            $scope.operationIcon = null;
 
         } else {
         	//Build input Stream on LED.
@@ -50,7 +53,9 @@ app.controller('calculatorController', function($scope,myCalc) {
     		$scope.pendingValue = null;
     		//Set pending operation to passed method
     		$scope.pendingOperation = method;
+            
     		$scope.pointerExists = 0;
+            this.setOperationIcon(method);
         }
         else{
         	//No previously entered Number
@@ -83,14 +88,13 @@ app.controller('calculatorController', function($scope,myCalc) {
     };
 
     $scope.clearOne = function(){
+        $scope.ledDisplay = $scope.ledDisplay.slice(0, -1);
+  	     if($scope.ledDisplay.length == 0){
 
-  	 $scope.ledDisplay = $scope.ledDisplay.slice(0, -1);
-  	 if($scope.ledDisplay.length == 0){
-
-  		$scope.ledDisplay = 0;
-  		$scope.pointerExists = 0;
-  	}
-  	$scope.pendingValue = parseFloat($scope.ledDisplay);
+  		    $scope.ledDisplay = 0;
+  		    $scope.pointerExists = 0;
+  	 }
+  	 $scope.pendingValue = parseFloat($scope.ledDisplay);
   }
 
 
@@ -99,7 +103,35 @@ app.controller('calculatorController', function($scope,myCalc) {
     $scope.pendingOperation = null;
     //Reset pointer flag. Allow user to add pointer.
     $scope.pointerExists = 0;
+    $scope.operationIcon = null;
+
   }
+
+  /**
+  * Show operations Icon on LEd
+  */
+
+  $scope.setOperationIcon = function(method){
+    if(method == "add"){
+        $scope.operationIcon = "+";
+    }
+    if(method == "subtract"){
+        $scope.operationIcon = "-";
+    }
+    if(method == "multiply"){
+        $scope.operationIcon = "*";
+    }
+    if(method == "divide"){
+        $scope.operationIcon = "/";
+    }
+    if(method == "mod"){
+        $scope.operationIcon = "%";
+    }
+
+  }
+
+
+
 
 
 });
